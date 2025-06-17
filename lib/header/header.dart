1c45302ebd55044c00b59d7cc72c024f78eb8b5a
Hernán Funes <hernan.funes@gmail.com>
2025-06-17 12:31:38 -0300

    Add profile picture to header

--- lib/header/header.dart
+++ lib/header/header.dart
@@ -3,26 +3,40 @@ import 'package:flutter/material.dart';
 class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(
-          'Hernán Javier Funes',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 39,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF003366),
+        ClipOval(
+          child: Image.asset(
+            'resources/me_simpson.png',
+            width: 80,
+            height: 80,
+            fit: BoxFit.cover,
           ),
         ),
-        SizedBox(height: 8.0),
-        Text(
-          'Software Engineer | Software Architect | Technical Lead',
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF4A4A4A),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Hernán Javier Funes',
+                style: TextStyle(
+                  fontSize: 39,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF003366),
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Software Engineer | Software Architect | Technical Lead',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4A4A4A),
+                ),
+              ),
+            ],
           ),
         ),
       ],
