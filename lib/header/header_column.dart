3985d320042ed8d1a2b067ac99d4fe8297d6ee63
Hernán Funes <hernan.funes@gmail.com>
2025-06-19 16:42:11 -0300

    Refactor header links to exclude emojis

--- lib/header/header_column.dart
+++ lib/header/header_column.dart
@@ -23,28 +23,52 @@ class HeaderColumn extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 8),
-        InkWell(
-          onTap: () => _launch('https://github.com/hfunescom'),
-          child: const Text(
-            '🐙 github.com/hfunescom',
-            style: TextStyle(
-              fontSize: 12,
-              color: Color(0xFF6A6A6A),
-              decoration: TextDecoration.underline,
+        Row(
+          children: [
+            const Text(
+              '🐙',
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0xFF6A6A6A),
+              ),
             ),
-          ),
+            const SizedBox(width: 4),
+            InkWell(
+              onTap: () => _launch('https://github.com/hfunescom'),
+              child: const Text(
+                'github.com/hfunescom',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color(0xFF6A6A6A),
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 8),
-        InkWell(
-          onTap: () => _launch('https://linkedin.com/in/hernan-javier-funes'),
-          child: const Text(
-            '💼 http://linkedin.com/in/hernan-javier-funes',
-            style: TextStyle(
-              fontSize: 12,
-              color: Color(0xFF6A6A6A),
-              decoration: TextDecoration.underline,
+        Row(
+          children: [
+            const Text(
+              '💼',
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0xFF6A6A6A),
+              ),
             ),
-          ),
+            const SizedBox(width: 4),
+            InkWell(
+              onTap: () => _launch('https://linkedin.com/in/hernan-javier-funes'),
+              child: const Text(
+                'http://linkedin.com/in/hernan-javier-funes',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Color(0xFF6A6A6A),
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
         ),
         const SizedBox(height: 16),
       ],
