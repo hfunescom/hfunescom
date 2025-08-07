ed5fac490692ee2d8928d3dfc3089479a85ab4a8
hfunescom <hfunescom@users.noreply.github.com>
2025-08-07 03:35:48 +0000

    deploy: 96b988ff81b3f64c6fefa098c1d0226e8fb12370

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2175008410"
+    serviceWorkerVersion: "1059408461"
   }
 });
